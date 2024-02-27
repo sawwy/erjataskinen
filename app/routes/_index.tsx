e132cd29b80f925f211ba1f21dec6ebe1ab9c385
Sawwy <kimmo.j.taskinen@gmail.com>
2024-02-27 12:26:05 +0200

    Added Tailwind and Prettier. Temp fix for Vite HRM and Tailwind in dev

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,28 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: 'New Remix App' },
     {
-      name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
+      name: 'description',
+      content: 'Welcome to Remix! Using Vite and Cloudflare!',
     },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className="text-4xl">Welcome to Remix (with Vite and Cloudflare)</h1>
       <ul>
         <li>
           <a
+            className="text-4xl"
             target="_blank"
             href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
             rel="noreferrer"
           >
-            Cloudflare Pages Docs - Remix guide
+            Cloudflare
           </a>
         </li>
         <li>
